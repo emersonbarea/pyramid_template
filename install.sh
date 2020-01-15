ac272dd2405cfcae0027318bdd8dcde42e939e15
Emerson Barea <emerson.barea@gmail.com>
2020-01-15 00:15:58 -0300

    comment

--- install.sh
+++ install.sh
@@ -12,17 +12,25 @@ welcome() {
 }
 
 update() {
+
+	printf '\n\e[1;32m%-6s\e[m\n' '-- Updating Linux ...'
 	sudo apt update;
 	sudo apt upgrade -y;
 	sudo apt -f install -y;
 	sudo apt autoremove -y;
+
 }
 
 install_Linux_reqs() {
+
+	printf '\n\e[1;32m%-6s\e[m\n' '-- Installing Linux prerequisites ...'
 	sudo apt install python3-pip postgresql postgresql-contrib postgresql-server-dev-all -y;
+
 }
 
 install_Python_reqs() {
+
+	printf '\n\e[1;32m%-6s\e[m\n' '-- Installing Python prerequisites ...'
 	sudo pip3 install -r requirements.txt;
 }
 
