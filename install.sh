37856b5b4b8a6fc1035af7651335b698f433d765
Emerson Barea <emerson.barea@gmail.com>
2020-01-14 23:56:46 -0300

    comment

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo apt install python3-pip postgresql postgresql-contrib postgresql-server-dev-all
+apt update
+apt upgrade -y
+apt -f install -y
+apt autoremove -y
+
+sudo apt install python3-pip postgresql postgresql-contrib postgresql-server-dev-all -y
 
 pip install -r requirements.txt
