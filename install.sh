6099061e177c3f76f4fa9bb347c7fd641f8425dc
Emerson Barea <emerson.barea@gmail.com>
2020-01-15 00:13:53 -0300

    comment

--- install.sh
+++ install.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt -f install -y
-sudo apt autoremove -y
+# author: Emerson Barea (emerson.barea@gmail.com)
 
-sudo apt install python3-pip postgresql postgresql-contrib postgresql-server-dev-all -y
+welcome() {
+	printf '\n%s\n' 'This script installs and configures all MiniSecBGP environment.
+                   Some requirements must be met:
+                   - Ubuntu 18 Server LTS (Bionic Beaver)
+                   - Internet access
+                   - "sudo" user
+                   Note: the user "minisecbgp" will be created, so it should not exist previously.'
+}
 
-sudo pip install -r requirements.txt
+update() {
+	sudo apt update;
+	sudo apt upgrade -y;
+	sudo apt -f install -y;
+	sudo apt autoremove -y;
+}
+
+install_Linux_reqs() {
+	sudo apt install python3-pip postgresql postgresql-contrib postgresql-server-dev-all -y;
+}
+
+install_Python_reqs() {
+	sudo pip3 install -r requirements.txt;
+}
+
+
+main() {
+
+	BUILD_DIR=$(pwd)
+
+	welcome;
+	update;
+	install_Linux_reqs;
+	install_Python_reqs;
+
+}
