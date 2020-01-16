3b1167f2cd4be930c6436ce2c6e37b6764600618
Emerson Barea <emerson.barea@gmail.com>
2020-01-16 16:49:20 -0300

    comment

--- install.sh
+++ install.sh
@@ -69,26 +69,35 @@ install_app() {
 configure_uwsgi() {
 
         printf '\n\e[1;33m%-6s\e[m\n' '-- Configuring uWSGI deamon ...'
-        printf '%s\n%s\n%s\n\n%s\n%s%s\n%s\n%s%s\n%s%s%s%s%s\n\n%s\n%s\n' \
-               '[Unit]' 'Description=uwsgi daemon' 'After=network.target' '[Service]' \
-               'User=' $WHOAMI 'Group=www-data' 'WorkingDirectory=' $BUILD_DIR \
-               'ExecStart=' $BUILD_DIR '/venv/bin/uwsgi --ini-paste-logged ' $BUILD_DIR '/development.ini' \
-               '[Install]' 'WantedBy=multi-user.target' | sudo tee /etc/systemd/system/uwsgi.service
+        printf '%s%s%s%s%s%s%s%s%s\n' \
+'[Unit]
+Description=uwsgi daemon
+After=network.target
+
+[Service]
+User=' $WHOAMI '
+Group=www-data
+WorkingDirectory=' $BUILD_DIR '
+ExecStart=' $BUILD_DIR '/venv/bin/uwsgi --ini-paste-logged ' $BUILD_DIR '/development.ini
+
+[Install]
+WantedBy=multi-user.target' | sudo tee /etc/systemd/system/uwsgi.service
 
 }
 
 configure_nginx() {
 
         printf '\n\e[1;33m%-6s\e[m\n' 'Configuring Nginx...'
-        printf '%s%s%s\n%s%s%s\n%s\n\n%s\n%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s\n%s\n' \
-               'upstream ' $PROJECT_NAME ' {' \
-               '    server unix:///tmp/' $PROJECT_NAME '.sock;' \
-               '}' \
-               'server {' \
-               '    listen 80;' \
-               '    server_name ' $IP_ADDRESSES ';' \
-               '    access_log ' $BUILD_DIR '/access.log;' \
-               '    location / {
+        printf '%s%s%s%s%s%s%s%s%s\n' \
+'upstream ' $PROJECT_NAME ' {
+        server unix:///tmp/MiniSecBGP.sock;
+}
+
+server {
+        listen 80;
+        server_name ' $IP_ADDRESSES ';
+        access_log ' $BUILD_DIR '/access.log;
+        location / {
                 proxy_set_header        Host $http_host;
                 proxy_set_header        X-Real-IP $remote_addr;
                 proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -101,7 +110,9 @@ configure_nginx() {
                 proxy_buffering         off;
                 proxy_temp_file_write_size 64k;
                 proxy_pass http://' $PROJECT_NAME ';
-                proxy_redirect          off;' '    }' '}' | sudo tee /etc/nginx/sites-available/$PROJECT_NAME
+                proxy_redirect          off;
+        }
+}' | sudo tee /etc/nginx/sites-available/$PROJECT_NAME
 
         sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled
         sudo nginx -t
