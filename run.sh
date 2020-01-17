f70e7999d5f3a4c34d51b56bdb7bff78d0dca4e4
Emerson Barea <emerson.barea@gmail.com>
2020-01-17 09:06:47 -0300

    comment

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pip install -e ".[testing]"
-pserve development.ini --reload
-
--- run_full.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip3 install -e ".[testing]"
-alembic -c development.ini revision --autogenerate -m "init"
-alembic -c development.ini upgrade head
-initialize_minisecbgp_db development.ini
-pytest
-pserve development.ini --reload
-
