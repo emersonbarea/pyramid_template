7ede7f39051f76f69d7b734c52436d8f5c66a39a
Tocha <emerson.barea@gmail.com>
2020-06-09 18:13:28 -0300

    comment

--- minisecbgp/scripts/initialize_db.py
+++ minisecbgp/scripts/initialize_db.py
@@ -49,24 +49,24 @@ def setup_models(dbsession):
     for topology_type in topology_types:
         dbsession.add(models.TopologyType(topology_type=topology_type))
 
-    link_agreement_c2p = models.LinkAgreement(agreement='customer to provider',
+    link_agreement_c2p = models.LinkAgreement(agreement='provider to customer (p2c)',
                                               description='Customer ASes pay ISPs (providers) for access to the rest of '
                                                           'the Internet, also known as transit. In this scenario, '
                                                           'customers ASes does not retransmit the routes published '
-                                                          'by transit ISPs.')
+                                                          'by transit ISPs. The format is: <provider-as> - <customer-as>')
 
-    link_agreement_p2p = models.LinkAgreement(agreement='provider to provider',
+    link_agreement_p2p = models.LinkAgreement(agreement='peer to peer - or provider to provider (p2p)',
                                               description='A p2p link connects two ISPs who have agreed to exchange '
-                                                          'traffic on a quid pro quo basis.')
+                                                          'traffic on a quid pro quo basis. The format is: <peer-as> - <peer-as>')
     dbsession.add(link_agreement_c2p)
     dbsession.add(link_agreement_p2p)
 
-    id_link_agreement_c2p = dbsession.query(models.LinkAgreement.id).filter_by(agreement='customer to provider')
+    id_link_agreement_c2p = dbsession.query(models.LinkAgreement.id).filter_by(agreement='provider to customer (p2c)')
     realistic_topology_link_agreement_c2p = models.RealisticTopologyLinkAgreement(id_link_agreement=id_link_agreement_c2p,
                                                                                   value='-1')
     dbsession.add(realistic_topology_link_agreement_c2p)
 
-    id_link_agreement_p2p = dbsession.query(models.LinkAgreement.id).filter_by(agreement='provider to provider')
+    id_link_agreement_p2p = dbsession.query(models.LinkAgreement.id).filter_by(agreement='peer to peer - or provider to provider (p2p)')
     realistic_topology_link_agreement_p2p = models.RealisticTopologyLinkAgreement(id_link_agreement=id_link_agreement_p2p,
                                                                                   value='0')
     dbsession.add(realistic_topology_link_agreement_p2p)
